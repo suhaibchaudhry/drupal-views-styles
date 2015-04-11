3f7e80d49dc333ec3f8ff70eda5b0ac24b962b7e
Suhaib Chaudhry <suhaib@uitoux.com>
2015-04-11 16:46:32 -0400

    Fix parsing for flags.

--- uitoux_accordion/js/zozo.accordion.trigger.js
+++ uitoux_accordion/js/zozo.accordion.trigger.js
@@ -8,9 +8,9 @@
                 contentHeight: parseFloat(settings.views_uitoux_accordion.content_height),
                 headerSize: parseFloat(settings.views_uitoux_accordion.header_size),
                 headerFontSize: parseFloat(settings.views_uitoux_accordion.header_font_size),
-                responsive: settings.views_uitoux_accordion.responsive,
+                responsive: settings.views_uitoux_accordion.responsive == "1",
                 minWindowWidth: parseFloat(settings.views_uitoux_accordion.min_window_width),
-                scrollable: settings.views_uitoux_accordion.scrollable,
+                scrollable: settings.views_uitoux_accordion.scrollable == "1",
                 animation: {
                     easing: settings.views_uitoux_accordion.animation_easing,
                     duration: parseInt(settings.views_uitoux_accordion.animation_duration)
